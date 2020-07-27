ff1acba377449334249be6a67d1f6f94a61eb3f1
moshir <moshir.mikael@gmail.com>
2020-07-27 17:33:14 +0200

    fix

--- src/main/scala/com/yotpo/metorikku/configuration/job/input/GlueTable.scala
+++ src/main/scala/com/yotpo/metorikku/configuration/job/input/GlueTable.scala
@@ -2,6 +2,7 @@ package com.yotpo.metorikku.configuration.job.input
 
 import com.yotpo.metorikku.configuration.job.InputConfig
 import com.yotpo.metorikku.input.Reader
+import com.yotpo.metorikku.input.readers.glue.GlueInput
 
 case class GlueTable(
                     database : String,
